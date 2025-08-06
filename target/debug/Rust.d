8ca2ccc8a9a92352ced8bae4cfbd0dd773bb1574
Anand <achaudharyskn@gmail.com>
2025-08-06 18:05:42 +0530

    control flow

---
+++ target/debug/Rust.d
@@ -0,0 +1,1 @@
+/mnt/d/Web3/Project/Rust/target/debug/Rust: /mnt/d/Web3/Project/Rust/src/main.rs
---
+++ target/debug/deps/Rust-f03b251a20640a38.d
@@ -0,0 +1,5 @@
+/mnt/d/Web3/Project/Rust/target/debug/deps/Rust-f03b251a20640a38.d: src/main.rs
+
+/mnt/d/Web3/Project/Rust/target/debug/deps/Rust-f03b251a20640a38: src/main.rs
+
+src/main.rs:
